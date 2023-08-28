b1169362a6c0127538c658280c91f42dc1eb081b
Kim Zeuner <kimzeuner@Kims-MacBook-Pro-2.local>
2023-08-28 12:06:43 +0200

    solution

--- Thanos.java
+++ Thanos.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class Thanos {
@@ -11,22 +13,28 @@ public class Thanos {
         heroes.add(new Hero("Captain America", 100));
         heroes.add(new Hero("Thor", 1501));
 
-        // TODO 1 : Modify Hero to implements Comparable and sort by name (ascending)
-
+        
+        Collections.sort(heroes); 
 
         System.out.println("\nOrder by name:");
         showList(heroes);
 
-        // TODO 2: Add a Comparator and sort by age (descending)
-
+    
+        Comparator<Hero> ageComparator = new Comparator<Hero>() {
+            @Override
+            public int compare(Hero h1, Hero h2) {
+                return Integer.compare(h2.getAge(), h1.getAge()); 
+            }
+        };
+        Collections.sort(heroes, ageComparator);
 
         System.out.println("\nOrder by age:");
         showList(heroes);
     }
 
     private static void showList(List<Hero> heroes) {
-        for (Hero heroe : heroes) {
-            System.out.println(heroe.getName() + ", " + heroe.getAge());
+        for (Hero hero : heroes) {
+            System.out.println(hero.getName() + ", " + hero.getAge());
         }
     }
 }
